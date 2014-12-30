30c23728296583d545b3ec05b5bbcc3322df87ec
Indu <pinschen@gmail.com>
2014-12-30 14:13:25 +0000

    Using window.onload = writeMessage; with InnerHTML

---
+++ OnloadStatements/statements.js
@@ -0,0 +1,15 @@
+window.onload = writeMessage;
+
+function writeMessage() {
+	//code
+	document.getElementById("hiMessage").innerHTML = "hi there";
+}
+
+/*
+<script type="text/javascript" >
+if (test) {
+	test = false;
+		alert(test);
+}
+</script>
+*/
